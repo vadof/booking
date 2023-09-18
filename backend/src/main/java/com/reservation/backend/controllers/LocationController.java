c43e6889a00846de0a807c79de81ac6d9e9503a3
Ida Tuule Jõgi <idjogi@ttu.ee>
2023-09-18 18:50:50 +0300

    added all Estonian cities

---
+++ backend/src/main/java/com/reservation/backend/controllers/LocationController.java
@@ -0,0 +1,25 @@
+package com.reservation.backend.controllers;
+
+import com.reservation.backend.entities.Location;
+import com.reservation.backend.services.LocationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/locations")
+public class LocationController {
+    private final LocationService locationService;
+
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping()
+    public List<Location> getAllLocations() {
+        return locationService.getAllLocations();
+    }
+
+}
