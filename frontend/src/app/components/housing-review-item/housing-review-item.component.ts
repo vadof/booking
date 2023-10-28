218a399a16f30dcd2f23372fc8f7b7f6af51db47
Ida Tuule <idjogi@taltech.ee>
2023-10-28 14:18:30 +0300

    reviews

---
+++ frontend/src/app/components/housing-review-item/housing-review-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {IReview} from "../../models/IReview";
+
+@Component({
+  selector: 'app-housing-review-item',
+  templateUrl: './housing-review-item.component.html',
+  styleUrls: ['./housing-review-item.component.scss']
+})
+export class HousingReviewItemComponent implements OnInit {
+  @Input() review!: IReview;
+
+  ngOnInit(): void {
+  }
+}
--- frontend/src/app/pages/housing-page/housing-page.component.ts
+++ frontend/src/app/pages/housing-page/housing-page.component.ts
@@ -5,6 +5,8 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {HousingService} from "../../services/housing.service";
 import {MatDateRangePicker} from "@angular/material/datepicker";
 import {FormControl, FormGroup} from "@angular/forms";
+import {IReview} from "../../models/IReview";
+import {IHousingPaginatedResponse} from "../../reponses/IHousingPaginatedResponse";
 
 @Component({
   selector: 'app-housing-page',
@@ -13,6 +15,15 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class HousingPageComponent implements OnInit {
   @Input() housing: IHousing | null = null;
+  //@Input() review: IReview | null = null;
+
+  currentPage: number = 0;
+  totalPages: number = 0;
+  contentSize: number = 10;
+
+  pages: number[] = []
+
+  reviews: IReview[] = [];
 
   @ViewChild('picker') datePicker!: MatDateRangePicker<Date>;
 
@@ -30,6 +41,7 @@ export class HousingPageComponent implements OnInit {
   ngOnInit(): void {
     if (!this.housing) {
       this.getHousing();
+      this.searchReviews();
     }
   }
 
@@ -40,5 +52,34 @@ export class HousingPageComponent implements OnInit {
         .then((housing) => this.housing = housing)
         .catch(() => this.router.navigate(['']));
     });
+  }
+
+  searchReviews() {
+    this.reviews = this.housing.reviews
+    let path = '/v1/reviews?';
+    let params: string[] = [];
+    params.push(`page=${this.currentPage}`)
+    params.push(`size=${this.contentSize}`)
+    params.push(`housing=${this.housing}`)
+
+    for (let i = 0; i < params.length; i++) {
+      path += params[i];
+      if (i !== params.length - 1) path += '&';
+    }
+
+
+    this.httpService.sendGetRequest(path).subscribe(
+      response => {
+        const paginatedResponse: IHousingPaginatedResponse = response as IHousingPaginatedResponse;
+        this.totalPages = paginatedResponse.totalPages;
+        this.reviews = paginatedResponse.data;
+        this.pages = Array.from({length: this.totalPages}, (_, i) => i);
+      }
+    )
+  }
+
+  changePage(page: number) {
+    this.currentPage = page;
+    this.searchReviews();
   }
 }
