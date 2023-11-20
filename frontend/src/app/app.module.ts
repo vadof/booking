a94f8dbd2635ad6140285dd74e082f8a518bb778
idjogi <idjogi@taltech.ee>
2023-11-20 16:29:30 +0000

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -26,6 +26,11 @@ import { HousingItemComponent } from './components/housing-item/housing-item.com
 import { MyPublicationsPageComponent } from './pages/my-publications-page/my-publications-page.component';
 import { HousingReviewItemComponent } from './components/housing-review-item/housing-review-item.component';
 import { HousingPublishFormComponent } from './components/housing-publish-form/housing-publish-form.component';
+import { HousingPreviewDeleteItemComponent } from './components/housing-preview-delete-item/housing-preview-delete-item.component';
+import {NgOptimizedImage} from "@angular/common";
+import { HousingDeleteConfirmationComponent } from './components/housing-delete-confirmation/housing-delete-confirmation.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatButtonModule} from "@angular/material/button";
 import {MatSliderModule} from "@angular/material/slider";
 import { BookingHistoryPageComponent } from './pages/booking-history-page/booking-history-page.component';
 import {BookingItemComponent} from "./components/booking-item/booking-item.component";
