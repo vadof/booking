7bd1b14e63cb5e85d838ae6892896277bdd5d504
edsmag <edsmag@gmail.com>
2023-11-27 23:00:07 +0200

    commit booking history

--- frontend/src/app/components/housing-preview-item/housing-preview-item.component.ts
+++ frontend/src/app/components/housing-preview-item/housing-preview-item.component.ts
@@ -2,6 +2,11 @@ import {Component, Input, OnInit} from '@angular/core';
 import {IHousing} from "../../models/IHousing";
 import {HttpService} from "../../services/http.service";
 import {HousingService} from "../../services/housing.service";
+import {ILocation} from "../../models/ILocation";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {coordinatesValidator} from "../../validators/Coordinates";
+import {positiveNumberValidator} from "../../validators/PositiveNumber";
+import {checkInCheckOutValidator} from "../../validators/CheckIn";
 
 @Component({
   selector: 'app-housing-preview-item',
@@ -12,6 +17,8 @@ export class HousingPreviewItemComponent implements OnInit {
   @Input() housing!: IHousing;
   @Input() nights: number = 1;
   totalPrice: number = 0;
+  locations: ILocation[] = [];
+
 
   constructor(
     private httpService: HttpService,
@@ -22,17 +29,54 @@ export class HousingPreviewItemComponent implements OnInit {
     this.totalPrice = Math.round(this.housing.pricePerNight * this.nights * 100) / 100;
   }
 
+  housingForm = new FormGroup({
+    name: new FormControl<string>('', Validators.required),
+    location: new FormControl<string>('', Validators.required),
+    coordinates: new FormControl<string>('', [Validators.required, coordinatesValidator]),
+    pricePerNight: new FormControl<string>('', [Validators.required, positiveNumberValidator]),
+    people: new FormControl<string>('', [Validators.required, positiveNumberValidator]),
+    check: new FormControl<string>('', [Validators.required, checkInCheckOutValidator]),
+    minRentalAge: new FormControl<string>('', [Validators.required, positiveNumberValidator]),
+    description: new FormControl<string>('', Validators.required),
+    rooms: new FormControl<string>('', [Validators.required]),
+    m2: new FormControl<string>('', [Validators.required, positiveNumberValidator]),
+    minNights: new FormControl<string>('', [Validators.required, positiveNumberValidator])
+  })
+
   addToFavourites(housingInput: IHousing) {
     if (housingInput) {
       this.housing = housingInput;
-      this.httpService.sendPostRequest(`/v1/favourites`, null).subscribe(
+      this.httpService.sendPostRequest(`/v1/favourites`, housingInput.id).subscribe(
         response => {
           this.housingService.housing = response as IHousing;
-          this.housingService.currentStep++;
         }, err => {
           console.log(err);
         }
       )
+    }
+  }
+
+  private getRequestObjectFromForm(): Object {
+    const locationId = +this.housingForm.controls.location.value!;
+    const location: ILocation = this.locations.filter((l) => l.id === locationId)[0];
+
+    const check: any = this.housingForm.controls.check.value?.split('-');
+    const checkIn: string = check[0].trim() + ':00';
+    const checkOut: string = check[1].trim() + ':00';
+
+    return {
+      name: this.housingForm.controls.name.value,
+      location: location,
+      coordinates: this.housingForm.controls.coordinates.value,
+      pricePerNight: this.housingForm.controls.pricePerNight.value,
+      people: this.housingForm.controls.people.value,
+      checkIn,
+      checkOut,
+      minAgeToRent: this.housingForm.controls.minRentalAge.value,
+      description: this.housingForm.controls.description.value,
+      rooms: this.housingForm.controls.rooms.value,
+      m2: Math.round(+this.housingForm.controls.m2.value!),
+      minNights: this.housingForm.controls.minNights.value,
     }
   }
 
