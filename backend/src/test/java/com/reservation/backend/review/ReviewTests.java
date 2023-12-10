57a22a929cfca1249bb52f576bc0606121d254cb
Ida Tuule <idjogi@taltech.ee>
2023-12-10 11:10:16 +0200

    location&review tests

--- backend/src/test/java/com/reservation/backend/review/ReviewTests.java
+++ backend/src/test/java/com/reservation/backend/review/ReviewTests.java
@@ -7,6 +7,7 @@ import com.reservation.backend.entities.Housing;
 import com.reservation.backend.entities.Review;
 import com.reservation.backend.entities.User;
 import com.reservation.backend.enums.Role;
+import com.reservation.backend.exceptions.AppException;
 import com.reservation.backend.mapper.ReviewMapper;
 import com.reservation.backend.mapper.ReviewMapperImpl;
 import com.reservation.backend.mapper.UserMapper;
@@ -24,9 +25,7 @@ import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -35,8 +34,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 
@@ -56,6 +54,9 @@ public class ReviewTests extends GenericTest {
     @Spy
     private ReviewMapper reviewMapper = new ReviewMapperImpl();
 
+    @Spy
+    private UserMapper userMapper;
+
     @InjectMocks
     private ReviewService reviewService;
 
@@ -66,10 +67,10 @@ public class ReviewTests extends GenericTest {
         ReflectionTestUtils.setField(reviewMapper, "userMapper", userMapper);
     }
 
-    User reviewer = new User(1L, "test", "test", "test@gmail.com", "test1234", List.of(), LocalDate.now(), Role.USER);
 
     @Test
-    void testSaveReview() {
+    @Transactional
+    void testSaveReviewSuccessful() {
         ReviewDTO reviewDTO = new ReviewDTO();
 
         User reviewer = User.builder().id(2L).email("email1").build();
@@ -87,15 +88,105 @@ public class ReviewTests extends GenericTest {
         assertNotNull(savedReview);
     }
 
+    @Test
+    @Transactional
+    void testSaveReviewHousingNotFound() {
+        ReviewDTO reviewDTO = new ReviewDTO();
+
+        User reviewer = User.builder().id(2L).email("email1").build();
+        User owner = User.builder().id(1L).email("email2").build();
+
+        Housing housing = Housing.builder().id(1L).build();
+        housing.setReviews(new ArrayList<>());
+        housing.setOwner(owner);
+        Housing housing2 = Housing.builder().id(2L).build();
+
+        mockAuthenticatedUser(reviewer);
+        Mockito.when(housingRepository.findByIdAndPublishedTrue(housing.getId())).thenReturn(Optional.of(housing));
+        Mockito.when(reviewRepository.save(any(Review.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        AppException exception = assertThrows(
+                AppException.class,
+                () -> reviewService.saveReview(housing2.getId(), reviewDTO)
+        );
+
+        assertEquals("Housing with id 2 not found", exception.getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getHttpStatus());
+
+        verify(housingRepository, times(1)).findByIdAndPublishedTrue(housing2.getId());
+        verify(reviewRepository, never()).save(any());
+        verify(reviewMapper, never()).toDto(any());
+    }
+
+    @Test
+    @Transactional
+    void testSaveReviewOwnerCanNotLeaveAReview() {
+        ReviewDTO reviewDTO = new ReviewDTO();
+
+        User owner = User.builder().id(1L).email("email2").build();
+
+        Housing housing = Housing.builder().id(1L).build();
+        housing.setReviews(new ArrayList<>());
+        housing.setOwner(owner);
+
+        mockAuthenticatedUser(owner);
+        Mockito.when(housingRepository.findByIdAndPublishedTrue(housing.getId())).thenReturn(Optional.of(housing));
+        Mockito.when(reviewRepository.save(any(Review.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        AppException exception = assertThrows(
+                AppException.class,
+                () -> reviewService.saveReview(housing.getId(), reviewDTO)
+        );
+
+        assertEquals("The owner cannot leave reviews", exception.getMessage());
+        assertEquals(HttpStatus.FORBIDDEN, exception.getHttpStatus());
+
+        verify(housingRepository, times(1)).findByIdAndPublishedTrue(housing.getId());
+        verify(reviewRepository, never()).save(any());
+        verify(reviewMapper, never()).toDto(any());
+    }
+
+    @Test
+    @Transactional
+    void testSaveReviewCanNotLeaveAnotherReview() {
+        User reviewer = User.builder().id(2L).firstname("test").lastname("test").email("email1").build();
+        User owner = User.builder().id(1L).email("email2").build();
+        UserDTO reviewerDTO = new UserDTO(1L, "test", "test", "email1");
+
+        ReviewDTO reviewDTO = new ReviewDTO(1L, "uus", 9, LocalDate.now(), reviewerDTO);
+
+
+        Housing housing = Housing.builder().id(1L).build();
+        Review review = new Review(1L, "uus", 9, LocalDate.now(), reviewer, housing);
+        housing.setReviews(List.of(review));
+        housing.setOwner(owner);
+
+        mockAuthenticatedUser(reviewer);
+        Mockito.when(housingRepository.findByIdAndPublishedTrue(housing.getId())).thenReturn(Optional.of(housing));
+        Mockito.when(reviewRepository.save(any(Review.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        AppException exception = assertThrows(
+                AppException.class,
+                () -> reviewService.saveReview(housing.getId(), reviewDTO)
+        );
+
+        assertEquals("Review has already been left", exception.getMessage());
+        assertEquals(HttpStatus.FORBIDDEN, exception.getHttpStatus());
+
+        verify(housingRepository, times(1)).findByIdAndPublishedTrue(housing.getId());
+        verify(reviewRepository, never()).save(any());
+        verify(reviewMapper, never()).toDto(any());
+    }
+
 
     @Test
     public void testFindReviewById() {
         Long reviewId = 1L;
-        UserDTO userDTO = new UserDTO(1L, "test", "test", "test@gmail.com");
+        UserDTO reviewerDTO = new UserDTO(1L, "test", "test", "test@gmail.com");
         User reviewer = new User(1L, "test", "test", "test@gmail.com", "test1234", List.of(), LocalDate.now(), Role.USER);
         Housing housing = new Housing();
         Review review = new Review(1L, "vaffa", 8, LocalDate.now(), reviewer, housing);
-        ReviewDTO reviewDTO = new ReviewDTO(1L, "vaffa", 8, LocalDate.now(), userDTO);
+        ReviewDTO reviewDTO = new ReviewDTO(1L, "vaffa", 8, LocalDate.now(), reviewerDTO);
         when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
         when(reviewMapper.toDto(review)).thenReturn(reviewDTO);
 
@@ -108,50 +199,143 @@ public class ReviewTests extends GenericTest {
     }
 
     @Test
+    public void testFindReviewByIdCanNotFind() {
+        UserDTO reviewerDTO = new UserDTO(1L, "test", "test", "test@gmail.com");
+        User reviewer = new User(1L, "test", "test", "test@gmail.com", "test1234", List.of(), LocalDate.now(), Role.USER);
+        Housing housing = new Housing();
+        Review review = new Review(1L, "vaffa", 8, LocalDate.now(), reviewer, housing);
+        ReviewDTO reviewDTO = new ReviewDTO(1L, "vaffa", 8, LocalDate.now(), reviewerDTO);
+        when(reviewMapper.toDto(review)).thenReturn(reviewDTO);
+
+        AppException exception = assertThrows(
+                AppException.class,
+                () -> reviewService.findReviewById(review.getId())
+        );
+
+        assertEquals("Review with id 1 not found", exception.getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getHttpStatus());
+
+        verify(reviewMapper, never()).toDto(any());
+    }
+
+    @Test
     @Transactional
     public void testUpdateReview() {
-        // Arrange
-        ReviewDTO updatedReviewDTO = new ReviewDTO(/* initialize with updated data */);
-        Review existingReview = new Review(/* initialize with existing data */);
-        User reviewer = new User(/* initialize with data */);
-        when(reviewRepository.findById(updatedReviewDTO.getId())).thenReturn(Optional.of(existingReview));
-        mockAuthenticatedUser(reviewer);
-//        when(reviewMapper.toDto(existingReview)).thenReturn(updatedReviewDTO);
+        User reviewer = User.builder().id(2L).firstname("test").lastname("test").email("email1").build();
+        User owner = User.builder().id(1L).email("email2").build();
+        UserDTO reviewerDTO = new UserDTO(2L, "test", "test", "email1");
 
-        // Act
+        Housing housing = Housing.builder().id(1L).build();
+        housing.setReviews(new ArrayList<>());
+        housing.setOwner(owner);
+
+        ReviewDTO updatedReviewDTO = new ReviewDTO(1L, "uus", 9, LocalDate.now(), reviewerDTO);
+        Review existingReview = new Review(1L, "vaffa", 8, LocalDate.now(), reviewer, housing);
+
+        mockAuthenticatedUser(reviewer);
+        when(housingRepository.findByIdAndPublishedTrue(housing.getId())).thenReturn(Optional.of(housing));
+        when(reviewRepository.findById(updatedReviewDTO.getId())).thenReturn(Optional.of(existingReview));
+        when(reviewRepository.save(any(Review.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
         ReviewDTO resultReviewDTO = reviewService.updateReview(updatedReviewDTO);
 
-        // Assert
-        assertNotNull(resultReviewDTO);
-        // Add more assertions based on your expected behavior
+        assertEquals(updatedReviewDTO, resultReviewDTO);
 
-        // Verify that the repository's save method was called once
         verify(reviewRepository, times(1)).save(existingReview);
         verify(reviewMapper, times(1)).toDto(existingReview);
     }
 
     @Test
     @Transactional
+    public void testUpdateReviewOnlyReviewerCanChangeTheReview() {
+        User reviewer = User.builder().id(2L).firstname("test").lastname("test").email("email1").build();
+        User randomUser = User.builder().id(3L).build();
+        User owner = User.builder().id(1L).email("email2").build();
+        UserDTO reviewerDTO = new UserDTO(2L, "test", "test", "email1");
+
+        Housing housing = Housing.builder().id(1L).build();
+        housing.setReviews(new ArrayList<>());
+        housing.setOwner(owner);
+
+        ReviewDTO updatedReviewDTO = new ReviewDTO(1L, "uus", 9, LocalDate.now(), reviewerDTO);
+        Review existingReview = new Review(1L, "vaffa", 8, LocalDate.now(), reviewer, housing);
+
+        mockAuthenticatedUser(randomUser);
+        when(housingRepository.findByIdAndPublishedTrue(housing.getId())).thenReturn(Optional.of(housing));
+        when(reviewRepository.findById(updatedReviewDTO.getId())).thenReturn(Optional.of(existingReview));
+        when(reviewRepository.save(any(Review.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        AppException exception = assertThrows(
+                AppException.class,
+                () -> reviewService.updateReview(updatedReviewDTO)
+        );
+
+        assertEquals("Access to update review denied", exception.getMessage());
+        assertEquals(HttpStatus.FORBIDDEN, exception.getHttpStatus());
+
+        verify(reviewRepository, never()).save(any());
+        verify(reviewMapper, never()).toDto(any());
+    }
+
+
+    @Test
+    @Transactional
     public void testDeleteReview() {
-        // Arrange
-        Long reviewId = 1L;
-        Review review = new Review(/* initialize with data */);
-        User reviewer = new User(/* initialize with data */);
-        when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
+        User reviewer = User.builder().id(2L).firstname("test").lastname("test").email("email1").build();
+        User owner = User.builder().id(1L).email("email2").build();
+        UserDTO reviewerDTO = new UserDTO(2L, "test", "test", "email1");
+
+        Housing housing = Housing.builder().id(1L).build();
+        housing.setReviews(new ArrayList<>());
+        housing.setOwner(owner);
+
+        Review review = new Review(1L, "vaffa", 8, LocalDate.now(), reviewer, housing);
+        ReviewDTO reviewDTO = new ReviewDTO(1L, "vaffa", 8, LocalDate.now(), reviewerDTO);
+
         mockAuthenticatedUser(reviewer);
-//        when(reviewService.getCurrentUserAsEntity()).thenReturn(reviewer);
-//        when(reviewMapper.toDto(review)).thenReturn(new ReviewDTO(/* initialize with data */));
+        when(housingRepository.findByIdAndPublishedTrue(housing.getId())).thenReturn(Optional.of(housing));
+        when(reviewRepository.save(any(Review.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(reviewRepository.findById(reviewDTO.getId())).thenReturn(Optional.of(review));
 
-        // Act
-        ReviewDTO deletedReviewDTO = reviewService.deleteReview(reviewId);
 
-        // Assert
-        assertNotNull(deletedReviewDTO);
-        // Add more assertions based on your expected behavior
+        ReviewDTO deletedReviewDTO = reviewService.deleteReview(review.getId());
 
-        // Verify that the repository's delete method was called once
+        assertEquals(reviewDTO, deletedReviewDTO);
+
         verify(reviewRepository, times(1)).delete(review);
         verify(reviewMapper, times(1)).toDto(review);
+    }
+
+    @Test
+    @Transactional
+    public void testDeleteReviewUnauthorizedUserCanNotDelete() {
+        User reviewer = User.builder().id(2L).firstname("test").lastname("test").email("email1").build();
+        User owner = User.builder().id(1L).email("email2").build();
+        UserDTO reviewerDTO = new UserDTO(2L, "test", "test", "email1");
+
+        Housing housing = Housing.builder().id(1L).build();
+        housing.setReviews(new ArrayList<>());
+        housing.setOwner(owner);
+
+        Review review = new Review(1L, "vaffa", 8, LocalDate.now(), reviewer, housing);
+        ReviewDTO reviewDTO = new ReviewDTO(1L, "vaffa", 8, LocalDate.now(), reviewerDTO);
+
+        mockAuthenticatedUser(owner);
+        when(housingRepository.findByIdAndPublishedTrue(housing.getId())).thenReturn(Optional.of(housing));
+        when(reviewRepository.save(any(Review.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(reviewRepository.findById(reviewDTO.getId())).thenReturn(Optional.of(review));
+
+        AppException exception = assertThrows(
+                AppException.class,
+                () -> reviewService.deleteReview(review.getId())
+        );
+
+        assertEquals("Access to delete review denied", exception.getMessage());
+        assertEquals(HttpStatus.FORBIDDEN, exception.getHttpStatus());
+
+        verify(reviewRepository, times(1)).findById(review.getId());
+        verify(reviewRepository, never()).delete(any());
+        verify(reviewMapper, never()).toDto(any());
     }
 }
 
