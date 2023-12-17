18af5abad24a5e4eb0c4660d5646adb1264e3572
Vadim <vafilo@ttu.ee>
2023-12-17 19:56:17 +0200

    CI/CD update

--- backend/src/main/java/com/reservation/backend/config/DatabaseInitializer.java
+++ backend/src/main/java/com/reservation/backend/config/DatabaseInitializer.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import java.math.BigDecimal;
 import java.sql.Time;
 
+// TODO REMOVE
 @Configuration
 @RequiredArgsConstructor
 public class DatabaseInitializer {
